8c4faf4974cb3fdfc4a1b083113aaec7d18ab865
qwxying <qwxying@gmail.com>
2020-05-29 17:28:00 +0800

    完成页面的wxml和wxss

---
+++ components/confirm/confirm.js
@@ -0,0 +1,16 @@
+Component({
+  properties: {
+    placeholder: {
+      type: String,
+      value: ""
+    },
+    visible: {
+      type: Boolean,
+      value: false
+    },
+    value: {
+      type: String,
+      value: ""
+    }
+  }
+})
