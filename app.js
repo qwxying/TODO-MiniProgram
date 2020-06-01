ef0e493af2f3fd5b340a9074ca773e1069ce24de
qwxying <qwxying@gmail.com>
2020-06-01 12:36:03 +0800

    http 封装 完成小程序一键登录

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-//app.js
 App({
-  onLaunch: function () {},
+  globalData: {
+    host: 'https://gp-server.hunger-valley.com',
+    app_id: "****************",
+    app_secret: "***************",
+    t_app_id: "***************",
+    t_app_secret: "*****************"
+  }
 })
